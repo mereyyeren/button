8baf2f28b744e10358779ffdf2dd913a6a3438b1
mereyyeren <merey.yeren@jihc.kz>
2022-04-15 16:30:35 +0600

    tadam

---
+++ Yeren Merey.py
@@ -0,0 +1,22 @@
+
+
+# count the number of characters
+# alfabit = {'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1}
+# print(alfabit)
+
+
+#multiply all the items
+# cities = ["Astana", "Almaty", "Taraz", "Semey"]
+# print(cities)
+# print(len(cities))
+# val =('cities.value')
+# print(val)
+# cities.append('Qaragandy')
+# print(cities)
+
+#key to a dictionary
+# add= {'0':'10','1':'20'}
+# print(add)
+# add['2'] = ('30')
+# print(add)
+
